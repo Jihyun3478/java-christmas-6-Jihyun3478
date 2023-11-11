3166ba17a2a36d2810143aaa02a18c76fd03edf8
Jihyun3478 <leejihyun3478@gmail.com>
2023-11-12 04:07:34 +0900

    fix(DateTest): 테스트 코드 수정

--- src/test/java/christmas/domain/DateTest.java
+++ src/test/java/christmas/domain/DateTest.java
@@ -5,14 +5,15 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class DateTest {
 
-    @DisplayName("1 이상 31 이하가 아닌 날짜에 대한 예외 처리")
+    @DisplayName("방문할 날짜가 1 이상 31 이하의 숫자가 아닌 경우에 대한 예외 처리")
     @ParameterizedTest
     @ValueSource(ints = {0, 32})
-    void RangeOutDate(int input) {
-        assertThatCode(() -> new Date(input))
+    void dateRangeOut(int input) {
+        assertThatThrownBy(() -> new Date(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
