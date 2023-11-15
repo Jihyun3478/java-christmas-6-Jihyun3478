6f952bd03bd864b794c86907cdf3c94599c88dac
Jihyun3478 <leejihyun3478@gmail.com>
2023-11-15 23:33:56 +0900

    refactor: 상수 위치 변경

--- src/main/java/christmas/domain/SpecialEvent.java
+++ src/main/java/christmas/domain/SpecialEvent.java
@@ -6,8 +6,8 @@ import java.util.List;
 import static christmas.view.constant.NumberConstant.DEFAULT_CHRISTMAS_DISCOUNT;
 
 public class SpecialEvent {
-    private final int discount;
     private static final List<Integer> star = Arrays.asList(3, 10, 17, 24, 25, 31);
+    private final int discount;
 
     public SpecialEvent(Date date) {
         this.discount = applySpecialEvent(date);
